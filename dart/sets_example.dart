a3566974002a7ab757926b66dd82558e57a8c133
shahnewazIbrahim <shahnewaz886@gmail.com>
2025-05-13 09:12:30 +0600

    only for course assignment

---
+++ dart/sets_example.dart
@@ -0,0 +1,15 @@
+
+void main() {
+  Set<int> setA = {1, 2, 3, 4, 5};
+  Set<int> setB = {4, 5, 6, 7, 8};
+
+  print('Elements of Set A:');
+  for (var element in setA) {
+    print(element);
+  }
+
+  print('Elements of Set B:');
+  for (var element in setB) {
+    print(element);
+  }
+}
